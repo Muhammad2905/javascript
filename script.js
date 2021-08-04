d57508b81f78475d5d317f0a1542fbb2919fc0c8
Muhammad <madinajojokiller@gmail.com>
2021-08-04 08:21:29 +0500

    master

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt('Ismingizni kiriting');
+console.log('Mening ismim ' + name);
+var year = +prompt('Yoshingizni kiriting');
+console.log('Men ' + year +' yoshdaman');
+var pr1 = +prompt('Misolni yeching 7 + 3 = ?');
+console.log('Misol 1: 7 + 3 = ' + (7 + 3) + ' Sizning javob ' + pr1);
+var pr2 = +prompt('Misolni yeching 9 - 1 = ?');
+console.log('Misol 2: 9 - 1 = ' + (9 - 1) + ' Sizning javob ' + pr2);
+var pr3 = +prompt('Misolni yeching 6 + 4 = ?');
+console.log('Misol 3: 6 + 4 = ' + (6 + 4) + ' Sizning javob ' + pr3);
+var pr4 = +prompt('Misolni yeching 5 / 5 = ?');
+console.log('Misol 4: 5 / 5 = ' + (5 / 5) + ' Sizning javob ' + pr4);
+var pr5 = +prompt('Misolni yeching 7 * 3 = ?');
+console.log('Misol 5: 7 * 3 = ' + (7 * 3) + ' Sizning javob ' + pr5);
+alert("Natijani ko'rish uchun konsolni oching")
